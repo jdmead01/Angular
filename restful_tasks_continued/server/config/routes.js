4d080a45f25434793040e4e8b59cd1e582198666
Jimmy Mead <jdmead@asu.edu>
2019-07-15 10:40:18 -0700

    Create test.css

---
+++ restful_tasks_continued/server/config/routes.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Task = mongoose.model("Task");
+const tasks = require("../controllers/tasks");
+
+module.exports = function (app) {
+
+    app.get("/tasks", function (request, response) {
+        tasks.showAll(request, response);
+    });
+
+    app.get("/tasks/:id", function (request, response) {
+        tasks.showOne(request, response);
+    });
+
+    app.post("/tasks", function (request, response) {
+        tasks.create(request, response);
+    });
+
+    app.put("/tasks/:id", function (request, response) {
+        tasks.update(request, response);
+    });
+
+    app.delete("/tasks/:id", function (request, response) {
+        tasks.destroy(request, response);
+    });
+
+};
